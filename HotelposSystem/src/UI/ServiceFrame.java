3f7fa8aa7475fd17589082e9e56f2a86a9fa767a
Shuang0420 <sxu1@andrew.cmu.edu>
2015-11-28 21:07:19 +1030

    Merge origin/master
    Conflicts:
    	HotelposSystem/src/hotelpossystem/Service.java

--- HotelposSystem/src/UI/ServiceFrame.java
+++ HotelposSystem/src/UI/ServiceFrame.java
@@ -1042,13 +1042,13 @@ public class ServiceFrame extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Service", "Time", "Price"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
