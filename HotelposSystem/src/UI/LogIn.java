0f0039cd0603c519c1c673fa7d44275206a61879
Lingyan Jiang <lynne326@hotmail.com>
2015-11-30 20:30:59 +1030

    fwdw

--- HotelposSystem/src/UI/LogIn.java
+++ HotelposSystem/src/UI/LogIn.java
@@ -1,18 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package UI;
 
-import hotelpossystem.dao.DatabaseConnection;
 import hotelpossystem.dao.UserDAOImplement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import javax.swing.JButton;
-import javax.swing.JInternalFrame;
-import javax.swing.JLabel;
 
 /**
  *
